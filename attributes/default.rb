221ba27d11e3aa270da2258590c327497a50f83d
Xabier de Zuazo <xabier@onddo.com>
2014-02-23 21:33:07 +0100

    Added CentOS support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,17 @@
 
 default['boxbilling']['download_url'] = 'http://www.boxbilling.com/version/latest.zip'
 
+default['boxbilling']['required_packages'] = %w{ unzip }
+default['boxbilling']['php_packages'] = case node['platform']
+  when 'redhat', 'centos', 'scientific', 'fedora', 'suse', 'amazon', 'oracle'
+    %w{ php-curl php-mcrypt php-mysql }
+  when 'debian', 'ubuntu'
+    %w{ php5-curl php5-mcrypt php5-mysql }
+  else
+    Chef::Log.warn("Unknown platform (#{node['platform']}), guessing required package names. You may need to set the node['boxbilling']['php']['packages'] attribute.")
+    %w{ php-curl php-mcrypt php-mysql }
+end
+
 default['boxbilling']['dir'] = '/srv/www/boxbilling'
 default['boxbilling']['server_name'] = node['fqdn']
 default['boxbilling']['server_aliases'] = nil
