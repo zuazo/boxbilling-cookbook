410cd01725c7737e72354405405f5d647fc56df7
Xabier de Zuazo <xabier@onddo.com>
2014-06-03 17:29:41 +0200

    boxbilling_api password read from EncryptedAttribute

--- providers/api.rb
+++ providers/api.rb
@@ -7,7 +7,7 @@ def get_admin_api_token
   db = BoxBilling::Database.new({
     :database => node['boxbilling']['config']['db_name'],
     :user     => node['boxbilling']['config']['db_user'],
-    :password => node['boxbilling']['config']['db_password'],
+    :password => Chef::EncryptedAttribute.load(node['boxbilling']['config']['db_password']),
   })
   db.get_admin_api_token || begin
     db.generate_admin_api_token
