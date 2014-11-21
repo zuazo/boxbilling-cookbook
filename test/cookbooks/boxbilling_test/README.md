60681c2e62a653b803f4e4c95317d2d685e6be51
Xabier de Zuazo <xabier@onddo.com>
2014-11-21 21:46:08 +0100

    Move test/kitchen/cookbooks/boxbilling_test to test/cookbooks/boxbilling_test

---
+++ test/cookbooks/boxbilling_test/README.md
@@ -0,0 +1,1 @@
+This cookbook is used with [test-kitchen](http://kitchen.ci/) to test the parent, [boxbilling](https://supermarket.getchef.com/cookbooks/boxbilling) cookbook.
--- test/cookbooks/boxbilling_test/metadata.rb
+++ test/cookbooks/boxbilling_test/metadata.rb
@@ -22,7 +22,8 @@ name             'boxbilling_test'
 maintainer       'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
-description      'Installs/Configures boxbilling_test'
+description 'This cookbook is used with test-kitchen to test the parent, '\
+            'boxbilling cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
